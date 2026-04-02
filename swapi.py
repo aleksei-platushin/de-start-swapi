7ed65e75cfb55f851611b4e0f609ea6c5f494497
Aleksei Platushin <zeldman.92@gmail.com>
2026-04-02 11:23:31 +0800

    Add class APIRequester

--- swapi.py
+++ swapi.py
@@ -1,2 +1,19 @@
 # swapi.py
+import requests
+
+
+class APIRequester:
+    def __init__(self, base_url: str):
+        self.base_url = base_url
+
+    def get(self) -> requests.Response:
+        try:
+            response = requests.get(self.base_url)
+            response.raise_for_status()
+            return response
+        except requests.HTTPError as httperror:
+            print(f"HTTP error occurred: {httperror}")
+            return response
+        except requests.RequestException as e:
+            print(f'Ошибка запроса: {e}')
 
